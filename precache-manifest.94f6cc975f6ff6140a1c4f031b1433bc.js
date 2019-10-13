b7ca7069028de39bf10d7e7fcee9d7f4edfb89ec
Emanuel Dirschedl <emanuel@dirschedl.net>
2019-10-13 13:04:04 +0200

    Updates

---
+++ precache-manifest.94f6cc975f6ff6140a1c4f031b1433bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e635d594078f567d24a5137f8a81012",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "58491d2b612a23ce6d8d",
+    "url": "/monsters-rolodex/static/css/main.065ee79f.chunk.css"
+  },
+  {
+    "revision": "437aa2a58e988a92cb19",
+    "url": "/monsters-rolodex/static/js/2.f443d71e.chunk.js"
+  },
+  {
+    "revision": "58491d2b612a23ce6d8d",
+    "url": "/monsters-rolodex/static/js/main.ac358582.chunk.js"
+  },
+  {
+    "revision": "62a90dcdf724b9ab481e",
+    "url": "/monsters-rolodex/static/js/runtime-main.034b5f56.js"
+  }
+]);
